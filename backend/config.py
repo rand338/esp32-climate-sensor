053e14e4f7e493f725b348c7ac9c22681c052c4c
rand338 <rand338@users.noreply.github.com>
2026-01-12 12:38:41 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,14 @@
 # config.py
 
-# API Key for the ESP32 (Must match config.h in the sketch)
-API_KEY = "MySecretIoTKey123"
+# API Key for the ESP32 (must match the one in config.h)
+API_KEY = "MeinGeheimesIoTKennwort123"
 
-# Name of the SQLite database file
-DB_NAME = "climate.db"
+# Name of the sql database
+DB_NAME = "klima.db"
 
-# Server Settings
-HOST = '0.0.0.0'  # 0.0.0.0 = Accessible within the network
+# Server settings
+HOST = '0.0.0.0'  # 0.0.0.0 = Erreichbar im Netzwerk
 PORT = 5000
-DEBUG = True      # True for development, False for production
+DEBUG = True      # True for developement, false for production
+
+TIME_FORMAT_12H = False  # Set to True for AM/PM format, False for 24h format
